361e3891de8f3e703c68c34ce02c6688d3127390
AnukaJapa <japaridze.ana96@gmail.com>
2023-01-27 11:17:53 +0100

    rinomina userApi

---
+++ ShopMuseoProgettoFinale/Controllers/UserApiController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShopMuseoProgettoFinale.Database;
+using ShopMuseoProgettoFinale.Models;
+
+namespace ShopMuseoProgettoFinale.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserApiController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            using (ApplicationDbContext db = new ApplicationDbContext())
+            {
+                List<Product> listaProdotti = db.Products.ToList<Product>();
+
+
+                return Ok(listaProdotti);
+            }
+        }
+        //----------------------------
+        public IActionResult Details(int id)
+        {
+            using(ApplicationDbContext db = new ApplicationDbContext())
+            {
+
+                Product productFound = db.Products.Where(p=>p.Id == id).FirstOrDefault();
+                if (productFound != null)
+                {
+                    return NotFound("l'articolo che hai cercato non esiste");
+                } else
+                {
+                    return Ok(productFound);
+                }
+
+            }
+        }
+        //-----------------------------------
+
+    }
+}
